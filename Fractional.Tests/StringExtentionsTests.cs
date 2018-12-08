0915b3eef9cb3a22bfb7e28a665be77ca05b527a
smahjoub <saifeddine.mahjoub@outlook.com>
2018-12-08 22:43:48 +0100

    Add Extension helpers for fractional parsing

---
+++ Fractional.Tests/StringExtentionsTests.cs
@@ -0,0 +1,40 @@
+using System;
+using Xunit;
+
+namespace Fractional.Tests
+{
+    public class StringExtentionsTests
+    {
+        [Theory]
+        [InlineData("7 3/4", true)]
+        [InlineData("-7 3/4", true)]
+        [InlineData("3/4", false)]
+        [InlineData("4", false)]
+        public void IsMixedFractionalTheory(string value, bool expected)
+        {
+            var result = StringExtentions.IsMixedFractional(value);
+
+            Assert.Equal(expected, result);
+        }
+
+
+        [Theory]
+        [InlineData("7 3/4", false)]
+        [InlineData("-7 3/4", false)]
+        [InlineData("3/4", true)]
+        [InlineData("4", true)]
+        [InlineData("-4", true)]
+        public void IsSimpleractionalTheory(string value, bool expected)
+        {
+            var result = StringExtentions.IsSimpleFractional(value);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void IsMixedFractional_InValid_Fractional_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => StringExtentions.IsMixedFractional(null));
+        }
+    }
+}
---
+++ Fractional/StringExtentions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Fractional
+{
+    public static class StringExtentions
+    {
+        private const string MIXED_FRACTIONAL_PATTERN = @"^(\-?\d+\s+\d+/\d+)?$";
+
+        private const string SIMPLE_FRACTIONAL_PATTERN = @"^(\-?\d+/\d+|\-?\d+)?$";
+
+
+        /// <summary>
+        /// Return if the actual string reprensent a mixed fractional 
+        /// </summary>
+        /// <param name="fractional">Input fractional string</param>
+        /// <returns>A boolean result</returns>
+        public static bool IsMixedFractional(this string fractional)
+        {
+            if(fractional == null)
+            {
+                throw new ArgumentNullException("The fractional string could not be null");
+            }
+            return Regex.IsMatch(fractional, MIXED_FRACTIONAL_PATTERN);
+        }
+
+
+        /// <summary>
+        /// Return if the actual string reprensent a simple fractional 
+        /// </summary>
+        /// <param name="fractional">Input fractional string</param>
+        /// <returns>A boolean result</returns>
+        public static bool IsSimpleFractional(this string fractional)
+        {
+            if (fractional == null)
+            {
+                throw new ArgumentNullException("The fractional string could not be null");
+            }
+
+            return Regex.IsMatch(fractional, SIMPLE_FRACTIONAL_PATTERN);
+        }
+    }
+}
