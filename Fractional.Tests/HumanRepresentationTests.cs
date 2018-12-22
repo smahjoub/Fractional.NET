d66822d1275d0bdc33dc2ab4b94659f5f04c5312
smahjoub <saifeddine.mahjoub@outlook.com>
2018-12-22 16:32:02 +0100

    add unit tests

--- Fractional.Tests/HumanRepresentationTests.cs
+++ Fractional.Tests/HumanRepresentationTests.cs
@@ -9,9 +9,25 @@ namespace Fractional.Tests
         [InlineData(5, 4, "1 1/4")]
         [InlineData(4, 4, "1")]
         [InlineData(0, 4, "0")]
-        public void HumanRepresentationTest(long inputNumerator, long inputDenominator, string expectedOutput)
+        public void NumeratorAndDenominatorToHumanRepresentationTest(long inputNumerator, long inputDenominator, string expectedOutput)
         {
             var f1 = new Fractional(inputNumerator, inputDenominator);
+
+            Assert.False(f1.IsNaN);
+
+            Assert.True(f1.HumanRepresentation == expectedOutput);
+        }
+
+
+        [Theory]
+        [InlineData(0.25d, "1/4")]
+        [InlineData(0.33333d, "1/3")]
+        [InlineData(7.75d, "7 3/4")]
+        [InlineData(1d, "1")]
+        [InlineData(0d, "0")]
+        public void DecimalToHumanRepresentationTest(decimal d, string expectedOutput)
+        {
+            var f1 = new Fractional(d, keepExcat: false);
 
             Assert.False(f1.IsNaN);
 
